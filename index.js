b19217d6edaf4b5f3dfd7f9230afef09c8bc2f4e
Ilya Vlasov <i.vlasov@me.com>
2020-07-05 20:30:26 +0300

    added simple html ui

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 
 const express = require('express');
 const shell = require('shelljs');
+const _ = require('lodash');
 const {
 		dbQuery,
 		closeDbConnection,
+		asyncForEach,
 	} = require('./lib/utils');
 
 const app = express();
@@ -34,6 +36,38 @@ app.use('/workbook', async(req, res, next)=>{
 		+' --dest-path="'+destFilePath+'"'
 	shell.exec(cmd);
 	res.download(destFilePath);
+});
+
+app.use('/', async(req, res, next)=>{
+	const params=req.query;
+	console.log(params);
+	const modelIds=[11, 9, 19];
+	let models=(await dbQuery([
+		'SELECT * FROM `model` t',
+		'WHERE t.`model_id` IN ('+modelIds.join(',')+')'
+	], []));
+	//console.log(models);
+	models=_.sortBy(models, m=>m.display_name);
+	
+	await asyncForEach(models, async(model)=>{
+		model.units=(await dbQuery([
+			'SELECT * FROM `unit` t',
+			'WHERE t.`unit_id` IN ('+model.unit_id+')'
+		], []));
+		model.units.forEach(unit=>{
+			unit.number=model.unit_id.split(',').indexOf(unit.unit_id+"")+1;
+		});
+		model.units=_.sortBy(model.units, u=>u.number);
+	});
+	let html='';
+	models.forEach(model=>{
+		html+='<h2>'+model.display_name+'</h2>'
+		model.units.forEach(unit=>{
+			html+='<a href="/workbook?model='+model.model_id+'&unit='+unit.unit_id+'">'+model.display_name+' Unit '+unit.number+'</a><br/>';
+		})
+	});
+	res.send(html);
+	res.end();
 });
 
 const server=app.listen(port, null, function() {
